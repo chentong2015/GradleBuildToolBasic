5de246e385f7a21f9d5eca977f82690fca83cae1
chen tong <chen2015tong@gmail.com>
2024-03-23 12:11:53 +0100

    create gradle build tool project

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void testMain() {
+        Assertions.assertTrue(true);
+    }
+}
