efc2d12dc2e2703da6094d21cda067639cbf9d63
tchen <chen2015tong@gmail.com>
2025-04-17 17:36:12 +0200

    finish java application plugin

--- custom-plugins/java-application-plugin/src/main/java/org/example/Main.java
+++ custom-plugins/java-application-plugin/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Debug mode");
+
+        String propertyValue = System.getProperty("greeting.language");
+        System.out.println(propertyValue);
     }
 }
