0f64aadaee8f832e0f79ac803583ec4a1d026df6
tchen <chen2015tong@gmail.com>
2025-04-17 14:59:55 +0200

    finish spring boot plugin

--- custom-plugins/spring-boot-plugin/src/main/java/org/example/BaseController.java
+++ custom-plugins/spring-boot-plugin/src/main/java/org/example/BaseController.java
@@ -8,6 +8,10 @@ public class BaseController {
 
     @GetMapping("/base")
     public String base() {
+        // 获取设置的系统属性的值
+        String propertyValue = System.getProperty("com.example.property");
+        System.out.println(propertyValue);
+
         return "Base page";
     }
 }
