7971bc7660af7a23f32bfa6ff5879d8211c01f86
tchen <chen2015tong@gmail.com>
2025-04-20 19:31:35 +0200

    update gradle tasks

--- custom-dependencies/src/main/java/org/example/Main.java
+++ custom-dependencies/src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
 
 import com.netflix.discovery.EurekaClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
 
 public class Main {
 
+    // 测试从依赖管理引入的依赖
     private EurekaClient eurekaClient;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 测试从本地目录引入的依赖
+        CloseableHttpClient httpClient = HttpClients.createDefault();
     }
 }
